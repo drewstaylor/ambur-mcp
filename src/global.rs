a207f9a72b102d50d87015515da677eedb983bfc
Drew Taylor <drew@chainofinsight.com>
2025-05-06 13:18:00 -0400

    cargo fmt

--- src/global.rs
+++ src/global.rs
@@ -1,6 +1,8 @@
 // Contract addresses
-pub static CONTRACT_MAINNET: &str = "archway13s2wjcgx4pxwq3kw66669jt8l4jm840hhhxk7ktpj7r98wc5hvnqraj6w3";
-pub static CONTRACT_CONSTANTINE: &str = "archway1y9twsp7sf4ae8jl5huhduv0lwxa0l4g4n9lyvnvjkp648udpwh0q9vycz4";
+pub static CONTRACT_MAINNET: &str =
+    "archway13s2wjcgx4pxwq3kw66669jt8l4jm840hhhxk7ktpj7r98wc5hvnqraj6w3";
+pub static CONTRACT_CONSTANTINE: &str =
+    "archway1y9twsp7sf4ae8jl5huhduv0lwxa0l4g4n9lyvnvjkp648udpwh0q9vycz4";
 
 // System Instructions
 pub static SERVER_INFO_DESCR: &str = r#"
