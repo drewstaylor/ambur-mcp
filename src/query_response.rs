365029f7727dd02d71185edb59d9894135e79543
Drew Taylor <drew@chainofinsight.com>
2025-05-07 21:16:44 -0400

    add missing file

---
+++ src/query_response.rs
@@ -0,0 +1,13 @@
+use philabs_cw721_marketplace::msg::{
+    CollectionOfferDetailsResponse, CollectionRoyaltiesResponse, DetailsResponse, ListResponse,
+};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AllResponse {
+    pub list_response: ListResponse,
+    pub details_response: DetailsResponse,
+    pub collection_royalties_response: CollectionRoyaltiesResponse,
+    pub collection_offer_details_response: CollectionOfferDetailsResponse,
+}
